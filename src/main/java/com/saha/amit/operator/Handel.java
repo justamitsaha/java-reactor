3a78b71efefacd3b22fdad98f87788efc15f01fb
justamitsaha <justamitsaha@gmail.com>
2023-04-27 18:32:31 +0530

    Handel operator

---
+++ src/main/java/com/saha/amit/operator/Handel.java
@@ -0,0 +1,25 @@
+package com.saha.amit.operator;
+
+import com.saha.amit.util.Util;
+import reactor.core.publisher.Flux;
+
+public class Handel {
+    public static void main(String[] args) {
+        Flux.range(1, 20)
+                .handle((integer, synchronousSink) -> {
+                    if (integer % 2 == 0) {
+                        synchronousSink.next(integer);
+                    } else {
+                        synchronousSink.next("Odd Number" + integer);
+                    }
+                }).subscribe(Util.onNext());
+
+        Flux.generate(synchronousSink -> {
+            synchronousSink.next(Util.faker().country().name());
+        }).map(Object::toString).handle((country, synchronousSink) -> {
+            synchronousSink.next(country);
+            if (country.equalsIgnoreCase("India"))
+                synchronousSink.complete();
+        }).subscribe(Util.onNext());
+    }
+}
