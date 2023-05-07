c449d6e1de96bfc0696ca121191d8b9db13c483e
justamitsaha <justamitsaha@gmail.com>
2023-05-07 18:12:58 +0530

    Default thread

---
+++ src/main/java/com/saha/amit/f_threadingScheduler/A_DefaultThreadPubSub.java
@@ -0,0 +1,29 @@
+package com.saha.amit.f_threadingScheduler;
+
+import com.saha.amit.util.Util;
+import reactor.core.publisher.Flux;
+
+public class A_DefaultThreadPubSub {
+    public static void main(String[] args) {
+
+        Flux<Object> flux = Flux.create(fluxSink -> {
+                    printThreadName("create");
+                    fluxSink.next(1);
+                })
+                .doOnNext(i -> printThreadName("next " + i));
+
+        flux.subscribe(s->printThreadName("sub"+ s));
+        Runnable runnable = () -> flux.subscribe(v -> printThreadName("sub " + v));
+
+        for (int i = 0; i < 2; i++) {
+            new Thread(runnable).start();
+        }
+
+        Util.sleepSeconds(5);
+
+    }
+
+    private static void printThreadName(String msg){
+        System.out.println(msg + "\t\t: Thread : " + Thread.currentThread().getName());
+    }
+}
