323f1d5b2d9162f5c12e361594c75cdce3effd55
justamitsaha <justamitsaha@gmail.com>
2023-05-10 07:37:42 +0530

    Multiple Subscriber Subscribe on

---
+++ src/main/java/com/saha/amit/f_threadingScheduler/C_MultipleSubscribeOn.java
@@ -0,0 +1,28 @@
+package com.saha.amit.f_threadingScheduler;
+
+import com.saha.amit.util.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class C_MultipleSubscribeOn {
+    public static void main(String[] args) {
+
+        Flux<Object> flux = Flux.create(fluxSink -> {
+                    printThreadName("create");
+                    fluxSink.next(1);
+                })
+                .subscribeOn(Schedulers.newParallel("Amit"))
+                .doOnNext(i -> printThreadName("next " + i));
+
+        flux
+                .doFirst(() -> printThreadName("doFirst2"))
+                .subscribeOn(Schedulers.boundedElastic())
+                .doFirst(() -> printThreadName("doFirst1"))
+                .subscribe(s -> printThreadName("sub" + s));
+        Util.sleepSeconds(5);
+    }
+
+    private static void printThreadName(String msg) {
+        System.out.println(msg + "\t\t: Thread : " + Thread.currentThread().getName());
+    }
+}
