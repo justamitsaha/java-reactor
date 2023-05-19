a625d0663d38988a95c6bae01359e36cc1bf345a
justamitsaha <justamitsaha@gmail.com>
2023-05-19 21:56:24 +0530

    Windo group by

---
+++ src/main/java/com/saha/amit/j_batching/B_Window.java
@@ -0,0 +1,36 @@
+package com.saha.amit.j_batching;
+
+import com.saha.amit.util.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class B_Window {
+    private static AtomicInteger atomicInteger = new AtomicInteger(1);
+
+    public static void main(String[] args) {
+
+        eventStream()
+                .window(3)
+                .flatMap(flux -> saveEvents(flux))
+                .subscribe(Util.subscriber());
+        Util.sleepSeconds(60);
+    }
+    private static Flux<String> eventStream(){
+        return Flux.interval(Duration.ofMillis(500))
+                .map(i -> "event"+i);
+    }
+
+    private static Mono<Integer> saveEvents(Flux<String> flux){
+        return flux
+                .doOnNext(e -> System.out.println("saving " + e))
+                .doOnComplete(() -> {
+                    System.out.println("saved this batch");
+                    System.out.println("-------------------");
+                })
+                .then(Mono.just(atomicInteger.getAndIncrement()));
+    }
+
+}
