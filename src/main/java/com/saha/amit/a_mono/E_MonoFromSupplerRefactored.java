f2038ab3c75567f7a89f8aefd4dbcb635381a0b6
justamitsaha <justamitsaha@gmail.com>
2024-04-29 18:40:25 +0530

    Update E_MonoFromSupplerRefactored.java
    Added more comments

--- src/main/java/com/saha/amit/a_mono/E_MonoFromSupplerRefactored.java
+++ src/main/java/com/saha/amit/a_mono/E_MonoFromSupplerRefactored.java
@@ -4,6 +4,14 @@ import com.saha.amit.util.Util;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+/* Here we we are moving our buisness logic in pipeline inside Mono
+We can see that 
+1 when we call the method with pipe line it executes imiddietly i.e. building pipeline happens quickly
+2 When we subscribe to it it happens lazily but the method after that is waiting whihc is somethig not
+expeced for reactive programming i.e. it should not be blocking
+3 When we call using subscribeOn(Schedulers.boundedElastic() then it behaves in a way expected by reactive 
+programing. We have to keep a sleep method at end to prevent current thread from exiting otherwise the current 
+thread on whihc the program is running will exit without letting subscribe complete  */
 public class E_MonoFromSupplerRefactored {
     public static void main(String[] args) {
         int i = 3;
@@ -17,7 +25,7 @@ public class E_MonoFromSupplerRefactored {
                 getName().subscribe(
                         Util.onNext()
                 );
-                getName();
+                getName();                  // Waiting till the above is completed
                 break;
             case 3:
                 getName();
@@ -25,7 +33,7 @@ public class E_MonoFromSupplerRefactored {
                         .subscribeOn(Schedulers.boundedElastic())
                         .subscribe(Util.onNext());
                 getName();
-                Util.sleepSeconds(3);
+                Util.sleepSeconds(3);       // Won't wait for above subscribe to complete
         }
     }
 
