ec88295d561d3449b7fa1751be9edd07465e24a1
justamitsaha <justamitsaha@gmail.com>
2024-05-05 13:54:22 +0530

    Added comments and made readable

--- src/main/java/com/saha/amit/c_flux_programitically/A_FluxCreateSink.java
+++ src/main/java/com/saha/amit/c_flux_programitically/A_FluxCreateSink.java
@@ -9,18 +9,19 @@ import java.util.function.Consumer;
 public class A_FluxCreateSink {
     public static void main(String[] args) {
 
+        //It accepts a consumer of Flux Sink
         Flux.create(fluxSink -> {
-           fluxSink.next(1);
-           fluxSink.next(2);
-           fluxSink.next(3);
+            fluxSink.next(1);           //Items emitted with Next
+            fluxSink.next(2);
+            fluxSink.next(3);
         }).subscribe(System.out::println);
-        
+
         Flux.create(fluxSink -> {
             String countryName = "";
             int count = 1;
             do {
                 countryName = Util.faker().country().name();
-                fluxSink.next(count+"--> "+countryName);
+                fluxSink.next(count + "--> " + countryName);
                 count++;
             } while (!countryName.trim().equalsIgnoreCase("india"));
             fluxSink.complete();
--- src/main/java/com/saha/amit/c_flux_programitically/B_FluxCreateRefactored.java
+++ src/main/java/com/saha/amit/c_flux_programitically/B_FluxCreateRefactored.java
@@ -4,23 +4,39 @@ import com.saha.amit.util.Util;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 
+import java.time.Duration;
 import java.util.function.Consumer;
 
 public class B_FluxCreateRefactored {
     public static void main(String[] args) {
-        NameProducer nameProducer = new NameProducer();
-        Flux.create(nameProducer).subscribe(System.out::println);
+        FluxSinkProducer fluxSinkProducer = new FluxSinkProducer();
+        Flux.create(fluxSinkProducer)
+                .delayElements(Duration.ofMillis(500))
+                .subscribe(System.out::println);
+
+        fluxSinkProducer.produce();
+        Util.sleepSeconds(12);
     }
 }
 
-class NameProducer implements Consumer<FluxSink<String>> {
+class FluxSinkProducer implements Consumer<FluxSink<String>> {
     private FluxSink<String> fluxSink;
+
     @Override
     public void accept(FluxSink<String> stringFluxSink) {
         this.fluxSink = stringFluxSink;
     }
-    public void produce(){
-        String name = Util.faker().gameOfThrones().character();
-        this.fluxSink.next(name);
+
+    //name can be anything for this method
+    public void produce() {
+        String name = "";
+        int count = 0;
+        do {
+            name = Util.faker().gameOfThrones().character();
+            count++;
+            this.fluxSink.next(count + " --> " + name);
+        } while (!name.equals("Muttering Bill") &&  count < 11);
+        fluxSink.complete();
+
     }
 }
