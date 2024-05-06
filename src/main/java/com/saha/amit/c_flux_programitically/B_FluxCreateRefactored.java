e36c3746e5e46acf8da7ddd448ad1612249bc686
justamitsaha <justamitsaha@gmail.com>
2024-05-06 18:58:44 +0530

    Update B_FluxCreateRefactored.java

--- src/main/java/com/saha/amit/c_flux_programitically/B_FluxCreateRefactored.java
+++ src/main/java/com/saha/amit/c_flux_programitically/B_FluxCreateRefactored.java
@@ -2,31 +2,42 @@ package com.saha.amit.c_flux_programitically;
 
 import com.saha.amit.util.Util;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 
-public class C_TakeOperator {
+import java.time.Duration;
+import java.util.function.Consumer;
+
+public class B_FluxCreateRefactored {
     public static void main(String[] args) {
-        Flux.create(fluxSink -> {
-            for(int i =0; i<5; i++){
-                fluxSink.next("hello "+i);
-                System.out.println(i);      // Even if reciever takes 3 the source will continue to execute
-            }
-        })
-            .take(3)
-            .subscribe(s -> System.out.println("Flux 1 -->"+ s));
+        FluxSinkProducer fluxSinkProducer = new FluxSinkProducer();
+        Flux.create(fluxSinkProducer)
+                .delayElements(Duration.ofMillis(500))
+                .subscribe(System.out::println);
 
-        //Problem with create is that it will continue to emit even subscription is cancelled,
-        so we have to add a condition for fluxSink.isCancelled()
+        //This is when flux will emit as next method is called here
+        fluxSinkProducer.produce();
+        Util.sleepSeconds(12);
+    }
+}
 
-        Flux.create(fluxSink -> {
-            String country = "";
-            do {
-                country = Util.faker().country().name();
-                fluxSink.next(country);
-                System.out.println("Flux 2 send-->"+ country)
-            } while (country.equalsIgnoreCase("INDIA") && fluxSink.isCancelled());
-            fluxSink.complete();
-        })
-                .take(3)
-                .subscribe(s -> System.out.println("Flux 2 recieved-->"+ s));
+class FluxSinkProducer implements Consumer<FluxSink<String>> {
+    private FluxSink<String> fluxSink;
+
+    @Override
+    public void accept(FluxSink<String> stringFluxSink) {
+        this.fluxSink = stringFluxSink;
+    }
+
+    //name can be anything for this method
+    public void produce() {
+        String name = "";
+        int count = 0;
+        do {
+            name = Util.faker().gameOfThrones().character();
+            count++;
+            this.fluxSink.next(count + " --> " + name);
+        } while (!name.equals("Muttering Bill") &&  count < 11);
+        fluxSink.complete();
+
     }
 }
