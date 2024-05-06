889abfca1b60d07759efa5f91cbd8ba6c3e75b89
justamitsaha <justamitsaha@gmail.com>
2024-05-06 19:00:18 +0530

    Update C_TakeOperator.java

--- src/main/java/com/saha/amit/c_flux_programitically/C_TakeOperator.java
+++ src/main/java/com/saha/amit/c_flux_programitically/C_TakeOperator.java
@@ -5,21 +5,28 @@ import reactor.core.publisher.Flux;
 
 public class C_TakeOperator {
     public static void main(String[] args) {
-        Flux.range(1, 10).log().take(3).log()
-                .subscribe(System.out::println);
+        Flux.create(fluxSink -> {
+            for(int i =0; i<5; i++){
+                fluxSink.next("hello "+i);
+                System.out.println(i);      // Even if reciever takes 3 the source will continue to execute
+            }
+        })
+            .take(3)
+            .subscribe(s -> System.out.println("Flux 1 -->"+ s));
 
-        //Problem with create is that it will continue to emit even subscription is cancelled, so we have to add a condition for fluxSink.isCancelled()
+        //Problem with create is that it will continue to emit even subscription is cancelled,
+        so we have to add a condition for fluxSink.isCancelled()
 
         Flux.create(fluxSink -> {
-            String country = Util.faker().country().name();
+            String country = "";
             do {
+                country = Util.faker().country().name();
                 fluxSink.next(country);
+                System.out.println("Flux 2 send-->"+ country)
             } while (country.equalsIgnoreCase("INDIA") && fluxSink.isCancelled());
             fluxSink.complete();
         })
-                .log()
                 .take(3)
-                .log()
-                .subscribe(Util.onNext());
+                .subscribe(s -> System.out.println("Flux 2 recieved-->"+ s));
     }
 }
