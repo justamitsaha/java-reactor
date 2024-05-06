c45e09c759b0ab08ea6d2a869249b1b4f93fe03b
justamitsaha <justamitsaha@gmail.com>
2024-05-06 19:00:38 +0530

    Update D_FluxGenerate.java

--- src/main/java/com/saha/amit/c_flux_programitically/D_FluxGenerate.java
+++ src/main/java/com/saha/amit/c_flux_programitically/D_FluxGenerate.java
@@ -3,6 +3,13 @@ package com.saha.amit.c_flux_programitically;
 import com.saha.amit.util.Util;
 import reactor.core.publisher.Flux;
 
+/*
+Genertae uses synchronous Sink . Syncronous sink will repeate the logic in loop 
+and we can emit only once per loop.
+If we try to emit more than once it will throw error.
+Also we don't have to worry about cancelling subscription if we say take(2) 
+subscription will be cancelled after 2
+*/
 public class D_FluxGenerate {
     public static void main(String[] args) {
         Flux.generate(synchronousSink -> {
@@ -12,6 +19,8 @@ public class D_FluxGenerate {
             //synchronousSink.next(country);
             if (country.equalsIgnoreCase("INDIA"))
                 synchronousSink.complete();
-        }).subscribe(Util.onNext());
+        })
+        .take(2)
+        .subscribe(Util.onNext());
     }
 }
