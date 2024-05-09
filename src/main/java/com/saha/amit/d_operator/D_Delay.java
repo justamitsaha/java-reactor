0b9d18ba6c0efeb288eb2f6229294ca138d3a5cc
justamitsaha <justamitsaha@gmail.com>
2024-05-09 17:38:40 +0530

    Update D_Delay.java

--- src/main/java/com/saha/amit/d_operator/D_Delay.java
+++ src/main/java/com/saha/amit/d_operator/D_Delay.java
@@ -5,7 +5,11 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
-
+/*
+When we put delay it delays the item. Then reactor doesn't emit all item at once 
+it maintains an internal buffer of 32 and send 32 items at once.
+This 32 is defined in Queue class in reactor.util.concurrent in property called reactor.bufferSize.x
+*/
 public class D_Delay {
     public static void main(String[] args) {
         System.setProperty("reactor.bufferSize.x", "9");
@@ -17,10 +21,10 @@ public class D_Delay {
         Flux.range(1, 10)
                 .log()
                 .map(i -> 10 / (5 - i))
-//                .onErrorReturn(-1)                // returns error
-//                .onErrorResume(e -> fallback())   // implements fallback
-                .onErrorContinue((err,obj)->{       // Continues execution
-                    System.out.println("Error"+ err +" for "+ obj);
+//                .onErrorReturn(-1)                // returns default value of -1 in case of error
+//                .onErrorResume(e -> fallback())   // implements fallback method to handel exception
+                .onErrorContinue((err,obj)->{       // Takes 2 parameter error and the actual object
+                    System.out.println("Error"+ err +" for "+ obj);//Handeles exception and continues exception
                 })
                 .subscribe(Util.subscriber());
 
