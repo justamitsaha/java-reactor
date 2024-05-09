1e7437f6372980197a7eb24de2e74b244f9d80e6
justamitsaha <justamitsaha@gmail.com>
2024-05-09 07:10:34 +0530

    Update B_CallBack.java

--- src/main/java/com/saha/amit/d_operator/B_CallBack.java
+++ src/main/java/com/saha/amit/d_operator/B_CallBack.java
@@ -16,17 +16,17 @@ public class B_CallBack {
                     fluxSink.error(new RuntimeException("oops"));
                     System.out.println("--completed");
                 })
-                .doOnComplete(() -> System.out.println("doOnComplete"))  // only when there is no error
-                .doFirst(() -> System.out.println("doFirst1")) //do first execute bottom to up so this executes 2nd
+                .doFirst(() -> System.out.println("doFirst1")) //do first execute bottom to up so this executes after below doFirst
                 .doFirst(() -> System.out.println("doFirst2"))   //1st items that executes
-                .doOnNext(o -> System.out.println("doOnNext : " + o))
                 .doOnSubscribe(s -> System.out.println("doOnSubscribe1" + s))  //next item before all do first
                 .doOnSubscribe(s -> System.out.println("doOnSubscribe2" + s))  //do On Subscribe executes top to bottom, so this executes 2nd
-                .doOnRequest(l -> System.out.println("doOnRequest : " + l))  //After subscribe
+                .doOnRequest(l -> System.out.println("doOnRequest : " + l))  //This will execute one time after that control will go to publisher
+                .doOnNext(o -> System.out.println("doOnNext : " + o))  // This will execute every time before publisher executes next
+                .doOnComplete(() -> System.out.println("doOnComplete"))  // only when there is no error
                 .doOnError(err -> System.out.println("doOnError : " + err.getMessage()))  //when there is an error
-                .doOnTerminate(() -> System.out.println("doOnTerminate"))  // On complete or on error
-                .doOnCancel(() -> System.out.println("doOnCancel"))
-                .doFinally(signal -> System.out.println("doFinally 1 : " + signal))
+                .doOnTerminate(() -> System.out.println("doOnTerminate"))  // After oncomplete/ error everytime except when take is called
+                .doOnCancel(() -> System.out.println("doOnCancel"))     // When subscriber cancels like take()
+                .doFinally(signal -> System.out.println("doFinally 1 : " + signal))  //Everytime
                 .doOnDiscard(Object.class, o -> System.out.println("doOnDiscard : " + o))  // in case of take operation discarded elements
                 .take(2)
                 .doFinally(signal -> System.out.println("doFinally 2 : " + signal))  // always executes
