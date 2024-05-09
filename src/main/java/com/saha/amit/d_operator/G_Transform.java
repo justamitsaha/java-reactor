7b2ffdc16f4044fecf60579dfe4f8625c6c55065
justamitsaha <justamitsaha@gmail.com>
2024-05-09 17:40:08 +0530

    Update G_Transform.java

--- src/main/java/com/saha/amit/d_operator/G_Transform.java
+++ src/main/java/com/saha/amit/d_operator/G_Transform.java
@@ -6,6 +6,11 @@ import reactor.core.publisher.Flux;
 
 import java.util.function.Function;
 
+/*
+It helps to build recuable code in our pipeline
+For example we have a reducdant code in multiple pipeline
+We can build the custom logic in a functiona and call them in different pipeline using transform
+*/
 public class G_Transform {
     public static void main(String[] args) {
 
@@ -25,6 +30,6 @@ public class G_Transform {
                 .filter(person -> person.getAge()>18)
                 .doOnNext(person -> person.setName(person.getName().toUpperCase()))
                 .doOnDiscard(Person.class,person -> System.out.println("Person not allowed "+ person.getName()+" of age "+ person.getAge()));
-
+                // doOnDiscard  will show all discarded items
     }
 }
