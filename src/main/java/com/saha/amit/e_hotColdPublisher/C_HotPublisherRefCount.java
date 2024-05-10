909bc3d478048c70df2fb1e56b7529cee956a1af
justamitsaha <justamitsaha@gmail.com>
2024-05-10 17:58:07 +0530

    Update C_HotPublisherRefCount.java

--- src/main/java/com/saha/amit/e_hotColdPublisher/C_HotPublisherRefCount.java
+++ src/main/java/com/saha/amit/e_hotColdPublisher/C_HotPublisherRefCount.java
@@ -5,13 +5,15 @@ import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 import java.util.stream.Stream;
-
+/*
+Another way to create hot publisher is using publish mehod and refCount
+publish method returns Connectable flux and ref count tells the number of
+subscribers need to join after which Publisher will start emitting
+For share method it was default as 1
+So shared() = .publish().refCount(1);
+*/
 public class C_HotPublisherRefCount {
     public static void main(String[] args) {
-
-        //shared() = .publish().refCount(1);
-
-
         Flux<String> movieStream = Flux.fromStream(() -> getMovie())
                 .delayElements(Duration.ofSeconds(1))
                 .publish()                  // Publish returns a connectable flux through which multiple subscriber can connect
@@ -19,9 +21,9 @@ public class C_HotPublisherRefCount {
 
         movieStream.subscribe(Util.subscriber("JHOLU"));
         Util.sleepSeconds(2);
-        movieStream.subscribe(Util.subscriber("BHOLU"));        // since ref count is 2 once BHOLU joins then only it stars emitting
+        movieStream.subscribe(Util.subscriber("BHOLU"));     // since ref count is 2 once BHOLU joins then only it stars emitting
         Util.sleepSeconds(2);
-        movieStream.subscribe(Util.subscriber("LOLU"));
+        movieStream.subscribe(Util.subscriber("LOLU"));       // misses out on some data
         Util.sleepSeconds(10);
 
     }
