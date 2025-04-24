4e1d25b94d5234db35a00aaab3cfb499a60f9062
justamitsaha <justamitsaha@gmail.com>
2025-04-24 07:48:45 +0530

    Documentation

--- src/main/java/com/saha/amit/a_mono/B_MonoJust.java
+++ src/main/java/com/saha/amit/a_mono/B_MonoJust.java
@@ -7,24 +7,36 @@ import reactor.core.publisher.Mono;
 public class B_MonoJust {
     public static void main(String[] args) {
         var mono = Mono.just("LOLO");
-
         System.out.println(mono);   //If we don't subscribe to mono it will not execute
+
+        /*
+       We don't have to request the no of items with request(n) method
+       like we do for custom  Subscriber as reactor framework does that for us with Long.MAX_VALUE (unbounded demand)
+        */
+        mono.subscribe(System.out::println);
+
+        mono.subscribe(
+                System.out::println,        //Success,
+                System.out::println,        //failure and error
+                () -> System.out.println("completed") //Completed
+        );
+
+
+        mono.subscribe(
+                value -> System.out.println("Received: "+ value),
+                error -> System.err.println("Error: "+ error),
+                () -> System.out.println("Completed"),
+                subscription -> subscription.request(1)  // Manually request 1 item
+        );
 
         SubscriberImpl subscriber = new SubscriberImpl();
         mono.subscribe(subscriber);
         subscriber.getSubscription().request(10);
 
 
-       /*
-       We don't have to request the no of items with request(n) method
-       like we do for custom  Subscriber as reactor framework does that for us
-        */
-        mono.subscribe(System.out::println);        //Only success
 
-        mono.subscribe(                                             //Success, failure and error
-                System.out::println,
-                System.out::println,
-                () -> System.out.println("completed")
-        );
+
+
+
     }
 }
